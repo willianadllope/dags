d8cdd8b87f4ec6113003be067640d1faa31535f3
willianadllope <willianadlopes@gmail.com>
2025-01-28 02:38:07 -0300

    configurando o config

--- dag_carga_inicial_full.py
+++ dag_carga_inicial_full.py
@@ -55,47 +55,47 @@ with DAG(
         )
         carga_custom_prod = BashOperator(
             task_id="carga_custom_prod",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_custom_prod' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_custom_prod' 'full'",
         )
         carga_cean_relacionado = BashOperator(
             task_id="carga_cean_relacionado",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_cean_relacionado' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_cean_relacionado' 'full'",
         )
         carga_grupo = BashOperator(
             task_id="carga_grupo",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_grupo' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_grupo' 'full'",
         )
         carga_grupo_custom_prod = BashOperator(
             task_id="carga_grupo_custom_prod",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_grupo_custom_prod' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_grupo_custom_prod' 'full'",
         )
         carga_grupo_config = BashOperator(
             task_id="carga_grupo_config",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_grupo_config' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_grupo_config' 'full'",
         )
         carga_clientes = BashOperator(
             task_id="carga_clientes",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_clientes' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_clientes' 'full'",
         )
         carga_tributos_internos_cache_st = BashOperator(
             task_id="carga_tributos_internos_cache_st",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache_st' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache_st' 'full'",
         )
         carga_tributos_internos_cache = BashOperator(
             task_id="carga_tributos_internos_cache",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache' 'full'",
         )
         carga_tributos_internos_cache_config = BashOperator(
             task_id="carga_tributos_internos_cache_config",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache_config' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_tributos_internos_cache_config' 'full'",
         )
         carga_envio = BashOperator(
             task_id="carga_envio",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_envio' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_envio' 'full'",
         )
         carga_schemafull = BashOperator(
             task_id="carga_schemafull",
-            bash_command="python /root/airflow/dags/scripts/call_procedure_carga_inicial.py 'pr_preparar_carga_schemafull' 'full'",
+            bash_command="python "+dag.params['scripts']+"call_procedure_carga_inicial.py 'pr_preparar_carga_schemafull' 'full'",
         )
         chain(carga_inicial_truncate, [
             carga_custom_prod,
@@ -110,7 +110,7 @@ with DAG(
             ], carga_envio, carga_schemafull )
     limpa_stage = BashOperator(
         task_id="limpa_stage",
-        bash_command="python /root/airflow/dags/scripts/call_snow_limpa_stage.py 'full'",
+        bash_command="python "+dag.params['scripts']+"call_snow_limpa_stage.py 'full'",
     )   
     chain(start_task, carrega_ids, limpa_stage, end_task)
 ### teste de sobe um restore do DBCarrefourAtualizacao
