c025abf2ef46a9e4c1349b4bb96f5d2262ab6bdc
willianadllope <willianadlopes@gmail.com>
2025-01-29 18:47:01 -0300

    geracao do parquet

--- scripts/parquet_geracao_envio.py
+++ scripts/parquet_geracao_envio.py
@@ -48,7 +48,7 @@ def export_query_to_parquet(sql,tipo, fileprefix, limit, strposicao='001'):
         t_step = time()
         current_date = datetime.now()
         formatted_previous_day = current_date.strftime("%Y%m%d%H%M%S")
-        file_name = '/parquet/tabelao/'+tipo+'/'+fileprefix+'/'+fileprefix + '_'+strposicao + '_'+str(i) +'_'+ formatted_previous_day+'.parquet'   
+        file_name = '/parquet2/tabelao/'+tipo+'/'+fileprefix+'/'+fileprefix + '_'+strposicao + '_'+str(i) +'_'+ formatted_previous_day+'.parquet'   
         df.to_parquet(file_name, index=False)
         lines += df.shape[0]
         print('  ', file_name, df.shape[0], f'lines ({round(time() - t_step, 2)}s)')
@@ -56,7 +56,7 @@ def export_query_to_parquet(sql,tipo, fileprefix, limit, strposicao='001'):
 
 def delete_files_directory(tipo, diretorio):
     # Specify the path of the directory to be deleted
-    directory_path = '/parquet/tabelao/'+tipo+'/'+diretorio
+    directory_path = '/parquet2/tabelao/'+tipo+'/'+diretorio
     # Check if the directory exists before attempting to delete it
     if os.path.exists(directory_path):
         shutil.rmtree(directory_path)
@@ -67,7 +67,7 @@ def delete_files_directory(tipo, diretorio):
 
 def send_parquet_snowflake(tipo, tabela):
     # populate the file_name and stage_name with the arguments
-    file_name = '/parquet/tabelao/'+tipo+'/'+tabela+'/*'
+    file_name = '/parquet2/tabelao/'+tipo+'/'+tabela+'/*'
     STAGE_SCHEMA = 'FULL'
     if(tipo=='INCREMENTAL'):
         STAGE_SCHEMA = 'INCREMENTAL'
