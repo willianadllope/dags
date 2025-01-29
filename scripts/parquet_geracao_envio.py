35c07890efedfbc6450ca855d7e1f76df8ecf796
willianadllope <willianadlopes@gmail.com>
2025-01-29 15:47:44 -0300

    geracao do parquet

--- scripts/parquet_geracao_envio.py
+++ scripts/parquet_geracao_envio.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from time import time
 from collections import namedtuple
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from datetime import datetime
 import pandas as pd
 import urllib as ul
@@ -24,7 +24,8 @@ props = ul.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
                                 "DATABASE=" + db['DATABASE'] + ";"
                                 "uid="+db['UID']+";pwd="+db['PWD']+";"
                                 "Encrypt=yes;TrustServerCertificate=yes;")
-con = create_engine("mssql+pyodbc:///?odbc_connect={}".format(props))
+##con = create_engine("mssql+pyodbc:///?odbc_connect={}".format(props))
+con = create_engine(f"mssql+pymssql://{db['UID']}:{db['PWD']}@{db['SERVER']}:{db['PORT']}/{db['DATABASE']}")
 
 
 connSnow = sf.connector.connect(
