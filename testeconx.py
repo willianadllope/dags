cb3d33ff5904cee54d2f94171502c4595b48de2c
willianadllope <willianadlopes@gmail.com>
2025-04-16 01:47:11 -0300

    incremental

--- testeconx.py
+++ testeconx.py
@@ -33,5 +33,9 @@ engine = create_engine(f"mssql+pymssql://{db['UID']}:{db['PWD']}@{db['SERVER']}:
 con = engine.connect().execution_options(stream_results=True)
 
 df = pd.read_sql("SELECT id, entidade from systax_app.dbo.clientes where id in (96201, 55982)", con)
-print(df[0])
+print(df)
+
+for index,row in df.iterrows():
+    print("id = ",row['id'])
+    print("nome = ",row['entidade'])
 
