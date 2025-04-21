9efdce2f020a72757830a553440c9ccd78f194fe
willianadllope <willianadlopes@gmail.com>
2025-04-21 18:03:58 -0300

    teste

--- teste_data_cleaning_dag.py
+++ teste_data_cleaning_dag.py
@@ -14,7 +14,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(seconds=5),
 }
 
 @dag(start_date=datetime(2025, 3, 6), schedule_interval=None, default_args=default_args)
