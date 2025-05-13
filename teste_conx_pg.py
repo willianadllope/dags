1018bd118196d7828b7da68d00019ccde86d1090
willianadllope <willianadlopes@gmail.com>
2025-05-13 12:57:46 -0300

    update full

---
+++ teste_conx_pg.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+connection = psycopg2.connect(database="systax", user="systax", password="SystX201406psql", host="dbcentralizada.cwvlwwjgliab.us-east-1.rds.amazonaws.com", port=5432)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * from tabelao_futuro_copia limit 10;")
+
+# Fetch all rows from database
+record = cursor.fetchall()
+
+print("Data from Database:- ", record)
+
