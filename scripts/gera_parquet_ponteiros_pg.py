00e15e62977fbac9e654ff0d0d1777b6dd52cc9e
willianadllope <willianadlopes@gmail.com>
2025-05-16 15:05:10 -0300

    teste

--- scripts/gera_parquet_ponteiros_pg.py
+++ scripts/gera_parquet_ponteiros_pg.py
@@ -38,7 +38,8 @@ def export_query_to_parquet(sql,pasta, fileprefix, limit, nrinicial):
         df.to_parquet(file_name, index=False)
         lines += df.shape[0]
         print('  ', file_name, df.shape[0], f'lines ({round(time() - t_step, 2)}s)')
-    print("  ", lines, f"lines exported {'' if i==0 else f' in {(i+1)} files'} ({round(time() - time_step, 2)}s)")
+    if i>0:
+        print("  ", lines, f"lines exported in {(i+1)} files' ({round(time() - time_step, 2)}s)")
     if lines < limit:
         lastID = -1
     return lastID
@@ -67,31 +68,35 @@ if len(sys.argv)  >= 2:
  
 print("Inicio: ",datetime.now())
 
+print("--------------------------------------------")
 print("criacao de novo registro de controle de ponteiros: ", datetime.now())
 cursor = con.cursor()
 cursor.execute("insert into controle_carga_ponteiros_snowflake (ponteiro, status) select max(menorts), 0 from tabelao;")
 con.commit()
 cursor.close()
 
+print("--------------------------------------------")
 print("captura do ultimo ponteiro: ", datetime.now())
 df = pd.read_sql("select id, ponteiro from controle_carga_ponteiros_snowflake where status = 2 order by datahora desc limit 1", con)
 for index,row in df.iterrows():
     idponteiro = row['id']
     ponteiro = row['ponteiro']
     print("id = ",idponteiro)
-    print("nome = ",ponteiro)
+    print("ponteiro = ",ponteiro)
 
 if apagararquivos == '1':
     print("apagando arquivos no diretorio", datetime.now())
     delete_files_directory(pastas['parquet']+'FULL/ajusteponteirords/')
 
+print("--------------------------------------------")
 print("download dos ponteiros", datetime.now())
 id = 0
 while id >= 0:
-    comando = "Select id, id_cliente, idconfigprod, menorts from public.tabelao where id > "+str(id)+" and menorts > "+str(ponteiro)+" and id < 20000000 order by id limit "+str(corte)
+    comando = "Select id, id_cliente, idconfigprod, menorts from public.tabelao where id > "+str(id)+" and menorts > "+str(ponteiro)+" and id < 10000000 order by id limit "+str(corte)
     id = export_query_to_parquet(comando, pastas['parquet']+'FULL/ajusteponteirords/', "regrasponteiros", paginacao, id)
 
 
+print("--------------------------------------------")
 print("atualiza carga atual para status=2: ", datetime.now())
 cursor = con.cursor()
 comando = "update controle_carga_ponteiros_snowflake set status = 2 where id = "+str(idponteiro)+";"
@@ -99,4 +104,5 @@ cursor.execute(comando)
 con.commit()
 cursor.close()
 
+print(" ")
 print("Fim: ",datetime.now())
