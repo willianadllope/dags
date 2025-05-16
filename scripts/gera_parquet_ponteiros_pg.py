a946dbbe6cf7773562ee77a548f1e8c84bf9e2ce
willianadllope <willianadlopes@gmail.com>
2025-05-16 13:32:30 -0300

    teste

--- scripts/gera_parquet_ponteiros_pg.py
+++ scripts/gera_parquet_ponteiros_pg.py
@@ -80,7 +80,7 @@ if apagararquivos == '1':
     delete_files_directory(pastas['parquet']+'FULL/ajusteponteirords/')
 
 idconfigprod = ' '
-comando = "Select id_cliente, idconfigprod, menorts from public.tabelao where idconfigprod > "+idconfigprod+" order by idconfigprod limit 10"
+comando = "Select id_cliente, idconfigprod, menorts from public.tabelao where idconfigprod > '"+idconfigprod+"' order by idconfigprod limit 10"
 print(comando)
 export_query_to_parquet(comando, pastas['parquet']+'FULL/ajusteponteirords/', "regrasponteiros", 2)
 
