64caa941843e55043755459ac90abf957d49fafa
willianadllope <willianadlopes@gmail.com>
2025-05-17 00:56:14 -0300

    teste

--- dag_geracao_csv_entrega.py
+++ dag_geracao_csv_entrega.py
@@ -17,9 +17,9 @@ class DAG_csv_to_rds:
     def delete_parquet(self):
         return BashOperator(
             task_id="delete_parquet",
-            bash_command="python "+self.dag.params['scripts']+"call_snow_procedure.py entrega pr_apaga_arquivos_ajusteponteirosrds",
+            #bash_command="python "+self.dag.params['scripts']+"call_snow_procedure.py entrega pr_apaga_arquivos_ajusteponteirosrds",
             dag=self.dag,
-            #bash_command="echo 'envia_tabelao_s3' ",
+            bash_command="echo 'delete_parquet' ",
         )   
 
     def get_parquet(self):
@@ -36,11 +36,19 @@ class DAG_csv_to_rds:
             dag=self.dag,
         )    
 
+    def carga_ajuste_ponteiro_rds(self):
+        return BashOperator(
+            task_id="carga_ajuste_ponteiro_rds",
+            bash_command="python "+self.dag.params['scripts']+"call_snow_procedure.py full PR_CARGA_AJUSTE_PONTEIRO_RDS",
+            dag=self.dag,
+        )   
+
     
     def create_dag(self):
       t0 = self.delete_parquet()
       t1 = self.get_parquet()
       t2 = self.send_parquet()
+      t3 = self.carga_ajuste_ponteiro_rds()
       t0 >> t1 >> t2
       return self.dag
 
