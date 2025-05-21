311c540e83d03a9fbfc6f7ffc55591b370cec417
willianadllope <willianadlopes@gmail.com>
2025-05-20 22:06:52 -0300

    teste

---
+++ scripts/send_s3_rds.py
@@ -0,0 +1,23 @@
+import boto3
+
+def list_files_in_s3_bucket(bucket_name, prefix=''):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                print(obj['Key'])
+        else:
+            print(f"No files found in bucket '{bucket_name}' with prefix '{prefix}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example usage:
+bucket_name = 's3://systaxbackuprds/'  # Replace with your bucket name
+list_files_in_s3_bucket(bucket_name)
+
+# To list files with a specific prefix (e.g., in a folder):
+prefix = 'pgentreganew/tabelao'  # Replace with your prefix
+list_files_in_s3_bucket(bucket_name, prefix)
+
+
