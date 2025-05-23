193fd26b146e8d2f7259cbab2179bb02323c5711
willianadllope <willianadlopes@gmail.com>
2025-05-23 01:32:49 -0300

    teste

--- scripts/send_s3_rds.py
+++ scripts/send_s3_rds.py
@@ -1,6 +1,17 @@
-import boto3
+import psycopg2
+import pandas as pd
 import config as cfg
+import boto3
+import sys
 import os
+import shutil
+from time import time
+from datetime import datetime
+
+pgentrega = config.pgentrega
+pastas = config.pastas 
+
+con = psycopg2.connect(database=pgentrega['DATABASE'], user=pgentrega['UID'], password=pgentrega['PWD'], host=pgentrega['SERVER'], port=pgentrega['PORT'])
 
 #awss3_key = os.environ.get('access_key')
 #awss3_secret = os.environ.get('secret_secret')
@@ -24,12 +35,25 @@ prefix = 'pgentreganew/tabelao'  # Replace with your prefix
 bucket_files = list_files_in_s3_bucket(bucket_name, prefix)
 
 index = 0
+
+
+print("Inicio: ",datetime.now())
+
+
 for file in bucket_files:
     if index==0:
         print("primeiro arquivo:", file)
+        cursor = con.cursor()
+        comando = "select fc_carrega_csv('usuarios','usuarios','"+file+"',1::int);"
+        cursor.execute(comando)
+        con.commit()
+        cursor.close()
+        print("--------------------------------------------")
     else:
         print(file)
     index = index + 1
 
 
 
+print(" ")
+print("Fim: ",datetime.now())
